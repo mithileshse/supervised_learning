dcb125d70d112377543cf2232267b12e00bd994d
mithilesh <mithileshse@gmail.com>
2020-12-07 00:43:03 +0530

    Adding the basic algorithm for KNN without any ML library

--- KNN/recomendation.py
+++ KNN/recomendation.py
@@ -3,25 +3,21 @@ import operator
 from os import listdir
 
 def file2matrix(filename):
-    love_dictionary = {'largeDoses': 3, 'smallDoses': 2, 'didntLike': 1}
     fr = open(filename)
-    arrayOLines = fr.readlines()
-    numberOfLines = len(arrayOLines)  # get the number of lines in the file
-    returnMat = zeros((numberOfLines, 3))  # prepare matrix to return
-    classLabelVector = []  # prepare labels return
+    numberOfLines = len(fr.readlines())
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector = []
+    fr = open(filename)
     index = 0
-    for line in arrayOLines:
+    for line in fr.readlines():
         line = line.strip()
         listFromLine = line.split('\t')
-        returnMat[index, :] = listFromLine[0:3]
-        if (listFromLine[-1].isdigit()):
-            classLabelVector.append(int(listFromLine[-1]))
-        else:
-            classLabelVector.append(love_dictionary.get(listFromLine[-1]))
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(listFromLine[-1])
         index += 1
-    return returnMat, classLabelVector
+    return returnMat,classLabelVector
 
-a,b=file2matrix('datingTestSet.txt')
+datingDataMat,datingLabels=file2matrix('datingTestSet.txt')
 
-print(a)
-print(b)
+print(datingLabels[0:20])
+print(datingDataMat)
